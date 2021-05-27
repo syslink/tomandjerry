1fb4a8028633b756c9ee700eafd3f206aa8aa74f
牙缝妹 <30425212+iwillhappy@users.noreply.github.com>
2021-05-27 18:22:26 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,111 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <div class="div1"><img src="../assets/img/img1.png" /></div>
+    <div class="div2">
+      <div class="top">
+        <div class="top_item">
+          <div class="one mar"><span>Balance</span><span>Locked</span></div>
+          <div class="one">
+            <span>TOM in circulation</span><span>161,231,222</span>
+          </div>
+        </div>
+        <div class="top_item">
+          <div class="one mar">
+            <span>Pending harvest</span><span>0.000</span>
+          </div>
+          <div class="one">
+            <span>Total supply</span><span>161,231,222</span>
+          </div>
+        </div>
+        <div class="top_item">
+          <div class="one mar"><span>TOM price</span><span>$7.356</span></div>
+          <div class="one"><span>TVL</span><span>161,231,222</span></div>
+        </div>
+        <div class="top_item">
+          <div class="one mar">
+            <span>TOM Market Cap</span><span>$723,356,330</span>
+          </div>
+          <div class="one">
+            <span>Volume（24hr）</span><span>161,231,222</span>
+          </div>
+        </div>
+        <div class="top_item">
+          <div class="one mar">
+            <span>NFT Transactions</span><span>356,330</span>
+          </div>
+          <div class="one">
+            <span>NFT Trading Vol</span><span>161,231,222</span>
+          </div>
+        </div>
+      </div>
+      <div class="center">Hottest Artworks in 2 Weeks</div>
+      <div class="bottom">
+        <img src="../assets/img/cat1.png" alt="cat" class="out" />
+        <img src="../assets/img/cat2.png" alt="cat" />
+        <img src="../assets/img/cat3.png" alt="cat" />
+        <img src="../assets/img/cat4.png" alt="cat" />
+        <img src="../assets/img/cat5.png" alt="cat" class="out" />
+        <img src="../assets/img/cat6.png" alt="cat" />
+        <img src="../assets/img/cat7.png" alt="cat" />
+        <img src="../assets/img/cat8.png" alt="cat" />
+      </div>
+      <div class="see">See More</div>
+    </div>
+    <div class="div3">
+      <div class="title">How To Buy NFT Artworks</div>
+      <div class="small_title">A new blockchain NFT e-commerce platform</div>
+      <div class="cat_bg">
+        <div class="bg1">
+          <span class="span1">Connect to Wallet</span>
+          <span class="span2"
+            >After downloading and installing your Metamask (Chrome) or
+            TokenPocket (app) wallet, set the wallet owner to BSC, and click
+            “Connect to a Wallet” on the upper right to log in to TOM, to start
+            your collection.</span
+          >
+        </div>
+        <div class="bg2">
+          <span class="span1">Purchase</span>
+          <span class="span2"
+            >Learn about the artworks, select your favorite item, click
+            “Purchase Now”, and complete the payment according to the
+            instructions. During the purchase process, you need to make sure
+            that the wallet has a sufficient balance in the corresponding
+            token.</span
+          >
+        </div>
+        <div class="bg3">
+          <span class="span1">Collection and Display</span>
+          <span class="span2"
+            >You can view the purchased items in TOM, move the NFT works to your
+            “wallet” for collection, or benefit from the “ Eco-TOM” for
+            additional income.</span
+          >
+        </div>
+        <div class="bg4">
+          <span class="span1">Global Express</span>
+          <span class="span2"
+            >TOM physical goods support globally express delivering. No matter
+            which country you are in, you can receive the goods after
+            purchase.</span
+          >
+        </div>
+      </div>
+    </div>
+    <div class="div4">
+      <div class="div_left">
+        <img src="../assets/img/left_img.png" style="margin-left: 20px" />
+        <div class="text">
+          <span>TOM.TT</span><span>Contact us by NFT@TOM.TT</span>
+        </div>
+      </div>
+      <div class="div_left right">
+        <span>Audit Report</span>
+        <span>Shopping Process</span>
+        <span>About Us</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,3 +114,6 @@ export default {
   components: {},
 };
 </script>
+<style scoped>
+@import "../assets/style/Home/Home.css";
+</style>
